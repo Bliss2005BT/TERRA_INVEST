b6efb3790d5c7850babe475825edfb673b07bac3
Bliss2005BT <tuscanobliss88@gmail.com>
2026-04-26 10:49:58 +0530

    Restore previous site typography and font imports

--- search-copy.php
+++ search-copy.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
   <title>Terra Invest Search</title>
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300;400;500;600;700&family=Outfit:wght@300;400;500;600&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&family=Inter:wght@300;400;500;600&family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
   <link rel="stylesheet" href="css/search.css">
   <script src="js/search.js" defer></script>
